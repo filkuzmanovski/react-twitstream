3ef70fd63f6a6a2b3bb146103d28f77513625f5b
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2016-06-03 12:13:23 -0700

    Application module pushed

--- source/app.js
+++ source/app.js
@@ -1,4 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var reactElement = React.createElement('h1');
-ReactDOM.render(reactElement, document.getElementById('react-application'));
+var Application = require('./components/Application.react');
+
+
+ReactDOM.render(<Application />, document.getElementById('react-application'));
