a242b48bcf7a4c88b1bc79b0f9c17aeb809d4eac
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2016-06-03 12:40:33 -0700

    Stream component added

---
+++ source/components/Stream.react.js
@@ -0,0 +1,45 @@
+var React = require('react');
+var SnapkiteStreamClient = require('snapkite-streamt-client');
+var StreamTweet = require('./StreamTweet.react');
+var Header = require('./Header.react');
+
+var Stream = React.createClass({
+
+	getInitialState: function () {
+		return {
+			tweet: null
+		}
+	},
+
+	componentDidMount: function () {
+		SnapkiteStreamClient.initializeStream(this.handleNewTweet);
+	},
+
+	componentWillUnmount: function () {
+		SnapkiteStreamClient.destroyStream();
+	},
+
+	handleNewTweet: function (tweet) {
+		this.setState({
+			tweet: tweet
+		});
+	},
+
+	render: function () {
+		var tweet = this.state.tweet;
+
+		if (tweet) {
+			return (
+				<StreamTweet
+				tweet={tweet}
+				onAddTweetToCollection={this.props.onAddTweetToCollection} />
+			);
+		}
+
+		return (
+		<Header text="Awaiting Twitter Photo Stream" />
+		);
+	}
+});
+
+module.exports = Stream;
